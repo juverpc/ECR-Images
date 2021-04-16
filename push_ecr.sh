022a2b7dc108972645754ed7475426d27fa7a6e0
juverpc <74871399+juverpc@users.noreply.github.com>
2021-04-16 18:07:08 -0500

    Update push_ecr.sh

--- push_ecr.sh
+++ push_ecr.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+CSV=${1:-'tags_name.csv'}
 ECR_URL="624733648801.dkr.ecr.us-east-1.amazonaws.com"
 ECR_REPO_NAME="demo"
 
-for i in sagemaker-safety-image sagemaker-fnb-image
+for i in $(cat ${CSV} | grep -v '#')
 do
 docker push ${ECR_URL}/${ECR_REPO_NAME}:${i}
 done
