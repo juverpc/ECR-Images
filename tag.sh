28d9713e657738f0370d2c7ddcc0214c2c841bc5
juverpc <74871399+juverpc@users.noreply.github.com>
2021-04-16 17:42:19 -0500

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+ECR_URL="624733648801.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REPO_NAME="demo"
 for i in sagemaker-safety-image sagemaker-fnb-image 
 do
-docker tag demo:${i} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/demo:${i}
+docker tag ${ECR_REPO_NAME}:${i} ${ECR_URL}/${ECR_REPO_NAME}:${i}
 done
