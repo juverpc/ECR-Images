325411048994eccb09b0ecad5a389421d2443687
Juver <juverpc@gmail.com>
2021-04-20 14:58:35 -0500

    update

--- push_ecr.sh
+++ push_ecr.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-CSV=${1:-'tags_name.csv'}
-ECR_REPO_NAME="demo"
+# CSV=${1:-'tags_name.csv'}
+ ECR_REPO_NAME="demo"
 
-for i in $(cat ${CSV} | grep -v '#')
-do
-docker push ${ECR_URL}/${ECR_REPO_NAME}:${i}
-done
+# for i in $(cat ${CSV} | grep -v '#')
+# do
+# docker push ${ECR_URL}/${ECR_REPO_NAME}:${i}
+# done
+#docker push ${ECR_URL}/${ECR_REPO_NAME}:sagemaker-safety-image
+docker push ${ECR_URL}/${ECR_REPO_NAME}:sagemaker-fnb-image
