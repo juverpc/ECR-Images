bdb1c8d4c876d1116eacd1d24d5c8e7d4a58afb7
Juver <juverpc@gmail.com>
2021-04-20 15:42:39 -0500

    update5

--- push_ecr.sh
+++ push_ecr.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# CSV=${1:-'tags_name.csv'}
- ECR_REPO_NAME="demo"
+CSV=${1:-'tags_name.csv'}
+ECR_REPO_NAME="demo"
 
-# for i in $(cat ${CSV} | grep -v '#')
-# do
-# docker push ${ECR_URL}/${ECR_REPO_NAME}:${i}
-# done
-docker push ${ECR_URL}/${ECR_REPO_NAME}:sagemaker-safety-image
-docker push ${ECR_URL}/${ECR_REPO_NAME}:sagemaker-fnb-image
+for i in $(cat ${CSV} | grep -v '#')
+do
+docker push ${ECR_URL}/${ECR_REPO_NAME}:${i}
+done
--- tag.sh
+++ tag.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# CSV=${1:-'tags_name.csv'}
- ECR_REPO_NAME="demo"
+CSV=${1:-'tags_name.csv'}
+ECR_REPO_NAME="demo"
 
-# #for i in sagemaker-safety-image sagemaker-fnb-image 
-# for i in $(cat ${CSV} | grep -v '#')
-# do
-# docker tag ${ECR_REPO_NAME}:${i} ${ECR_URL}/${ECR_REPO_NAME}:${i}
-# done
-docker tag ${ECR_REPO_NAME}:sagemaker-safety-image ${ECR_URL}/${ECR_REPO_NAME}:sagemaker-safety-image
-docker tag ${ECR_REPO_NAME}:sagemaker-fnb-image ${ECR_URL}/${ECR_REPO_NAME}:sagemaker-fnb-image
+
+for i in $(cat ${CSV} | grep -v '#')
+do
+docker tag ${ECR_REPO_NAME}:${i} ${ECR_URL}/${ECR_REPO_NAME}:${i}
+done
